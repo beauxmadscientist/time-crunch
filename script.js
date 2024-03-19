1f9f6c3e7ef0cd10936f48ce4a6f91927fcc0cbb
beauxmadscientist <beauxmadscientist@users.noreply.github.com>
2024-03-19 10:25:30 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const el = document.querySelector("p");
+let t = 30;
+el.innerHTML = t;
+var x = setInterval(() => {
+    t--;
+    if (t === 0) {
+        clearInterval(x);
+        el.innerHTML = "Game Over";
+    } else {
+        el.innerHTML = t;
+    }
+}, 1000);
